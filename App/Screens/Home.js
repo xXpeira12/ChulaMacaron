6b64f4680e4bd2d53e4b11248f114e771527cf1d
chulinuwuw <ananzxzoxd@gmail.com>
2024-04-20 02:05:05 +0700

    pls be the last initial file

--- App/Screens/Home.js
+++ App/Screens/Home.js
@@ -2,6 +2,7 @@ import { View, Text , Dimensions } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import Header from '../Components/Home/header';
 
+
 const { height } = Dimensions.get('window')
 
 export default function Home({ navigation }) {
@@ -11,7 +12,7 @@ export default function Home({ navigation }) {
 
   return (
     <View style={{backgroundColor:'#D5D5D5', height: height*0.5, justifyContent: 'center', alignItems: 'center'}}>
-      <Header />
+    <Header/>
     </View>
   )
 }
