06d14fbc1db98eb353aa1cd451388764fe2903fb
chulinuwuw <ananzxzoxd@gmail.com>
2024-04-20 00:11:28 +0700

    App folder / not finish Navigation initial file yet

--- App/Screens/ReportScreen.js
+++ App/Screens/ReportScreen.js
@@ -1,10 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function Profile() {
+export default function Report() {
   return (
     <View>
-      <Text>Profile</Text>
+      <Text>Report</Text>
     </View>
   )
 }
---
+++ App/์Navigations/TabNavigation.js
@@ -0,0 +1,18 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from '../Screens/Home';
+import Dashboard from '../Screens/Dashboard';
+import ReportScreen from '../Screens/ReportScreen';
+
+export default function TabNavigation() {
+    const Tab = createBottomTabNavigator();
+  return (
+    <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Report" component={ReportScreen} />
+        <Tab.Screen name="Dashboard" component={Dashboard} />
+
+    </Tab.Navigator>
+  )
+}
