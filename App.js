93818e5921af01445d485a5a709995738b22afd9
chulinuwuw <ananzxzoxd@gmail.com>
2024-04-20 21:26:54 +0700

    Navigation success

--- App.js
+++ App.js
@@ -1,36 +1,54 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import TabNavigation from "./App/Navigations/TabNavigation";
-import { NavigationContainer } from "@react-navigation/native";
-import { useFonts } from "expo-font";
-import * as SplashScreen from "expo-splash-screen";
-import React from "react";
-import { createStackNavigator } from "@react-navigation/stack";
-import Dashboard from "./App/Screens/Dashboard"; // adjust the path according to your project structure
-import Detail from "./App/Screens/Detail"; // adjust the path according to your project structure
-import Home from "./App/Screens/Home";
-import ReportScreen from "./App/Screens/ReportScreen";
-import ProblemScreen from "./App/Screens/ProblemScreen";
+import React , {useState}from 'react';
+import { StyleSheet, Button, View, Text, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { useNavigation } from '@react-navigation/native';
+import Dashboard from './App/Screens/Dashboard';
+import Detail from './App/Screens/Detail';
+import Home from './App/Screens/Home';
+import ReportScreen from './App/Screens/ReportScreen';
+import ProblemScreen from './App/Screens/ProblemScreen';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const Stack = createStackNavigator();
-// SplashScreen.preventAutoHideAsync()
+
+function NavigationButtons() {
+  const navigation = useNavigation();
+
+  const [focusedButton, setFocusedButton] = useState('Home');
+
+  const handlePress = (screenName) => {
+    setFocusedButton(screenName);
+    navigation.navigate(screenName);
+  };
+
+  return (
+    <View style={{ flexDirection: 'row', justifyContent: 'space-around', height:75, marginTop:15 }}>
+    <TouchableOpacity onPress={() => handlePress('Home')} activeOpacity={0.7}>
+      <Icon name="home-outline" size={30} color={focusedButton === 'Home' ? '#E26199' : '#000'} />
+      <Text style={{ fontWeight: 'bold', color: focusedButton === 'Home' ? '#E26199' : '#000' }}>Home</Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={() => handlePress('ReportScreen')} activeOpacity={0.7}>
+      <Icon name="document-text-outline" size={30} color={focusedButton === 'ReportScreen' ? '#E26199' : '#000'} />
+      <Text style={{ fontWeight: 'bold', color: focusedButton === 'ReportScreen' ? '#E26199' : '#000' }}>Report</Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={() => handlePress('Dashboard')} activeOpacity={0.7}>
+      <Icon name="analytics-outline" size={30} color={focusedButton === 'Dashboard' ? '#E26199' : '#000'} />
+      <Text style={{ fontWeight: 'bold', color: focusedButton === 'Dashboard' ? '#E26199' : '#000' }}>Dashboard</Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={() => handlePress('ProblemScreen')} activeOpacity={0.7}>
+      <Icon name="alert-circle-outline" size={30} color={focusedButton === 'ProblemScreen' ? '#E26199' : '#000'} />
+      <Text style={{ fontWeight: 'bold', color: focusedButton === 'ProblemScreen' ? '#E26199' : '#000' }}>Problem</Text>
+    </TouchableOpacity>
+  </View>
+  );
+}
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    chulaReg: require("./assets/fonts/CHULALONGKORNReg.otf"),
-    chulaBold: require("./assets/fonts/CHULALONGKORNBold.otf"),
-  });
-
-  // const onLayoutRootView = useCallback(async () => {
-  //   if (fontsLoaded){
-  //     await SplashScreen.hideAsync();
-  //   }
-  // },[fontsLoaded]);
-
   return (
     <View style={styles.container}>
       <NavigationContainer>
-        {/* <TabNavigation /> */}
+       
         <Stack.Navigator>
           <Stack.Screen name="ReportScreen" component={ReportScreen} />
           <Stack.Screen name="Dashboard" component={Dashboard} />
@@ -38,6 +56,7 @@ export default function App() {
           <Stack.Screen name="Home" component={Home} />
           <Stack.Screen name="Detail" component={Detail} />
         </Stack.Navigator>
+        <NavigationButtons />
       </NavigationContainer>
     </View>
   );
--- App/Screens/Home.js
+++ App/Screens/Home.js
@@ -20,16 +20,16 @@ export default function Home({ navigation }) {
      
 
     <Image source={Macaron} style={{width:'100%', resizeMode:'center'}}></Image>
-    <Text style={{fontSize:30, textAlign:'center' ,fontFamily: 'chulaBold' , paddingTop:0, color:'#E26199'}}>CHULA MACARON</Text>
-    <Text style={{fontSize:16, textAlign:'center', fontFamily: 'chulaReg', color:'#E26199'}}>make Nisits' lives better</Text>
+    <Text style={{fontSize:30, textAlign:'center' , paddingTop:0, color:'#E26199'}}>CHULA MACARON</Text>
+    <Text style={{fontSize:16, textAlign:'center', color:'#E26199'}}>make Nisits' lives better</Text>
 
     <View style={{flexDirection: screenWidth > 769 ? 'row' : 'column'}}>
     <View style={{width: screenWidth > 769 ? '50%' : '100%',}}>
     {/* <Text style= {{paddingTop:20, color:'#E26199', fontFamily: 'chulaBold'}}>You can report any problems around Chulalongkorn University.</Text> */}
 
     <View style= {{paddingTop:20}}>
-      <Text style= {{fontFamily: 'chulaBold' , color:'#E26199' ,fontSize:25}}>แพลตฟอร์ม</Text>
-      <Text style= {{fontFamily: 'chulaBold' , color:'#E26199' ,fontSize:25, paddingBottom:10}}>แจ้งและจัดการปัญหาในจุฬา</Text>
+      <Text style= {{color:'#E26199' ,fontSize:25}}>แพลตฟอร์ม</Text>
+      <Text style= {{color:'#E26199' ,fontSize:25, paddingBottom:10}}>แจ้งและจัดการปัญหาในจุฬา</Text>
     </View>
 
     <TouchableOpacity onPress={() => {}} style={{ backgroundColor: '#E26199', padding: 20, alignItems: 'center',  borderRadius: 10 ,
@@ -43,12 +43,12 @@ export default function Home({ navigation }) {
     elevation: 5,
     width: screenWidth > 769 ? '40%' : '100%',
   }}>
-    <Text style={{ color: 'white', fontFamily: 'chulaBold', fontSize: 18 }}>แจ้งปัญหาได้ที่นี่ (Click Here)</Text>
+    <Text style={{ color: 'white', fontSize: 18 }}>แจ้งปัญหาได้ที่นี่ (Click Here)</Text>
     </TouchableOpacity>
     </View>
 
     <View style={{flexDirection: screenWidth > 769 ? 'column':'column' , paddingTop:20 , width: screenWidth > 769 ? '50%' : '100%'}}>
-      <Text style={{fontFamily: 'chulaBold' ,fontSize:25 , color:'#E26199', paddingBottom:10}}>Overview</Text>
+      <Text style={{ fontSize:25 , color:'#E26199', paddingBottom:10}}>Overview</Text>
   
       <View style={{
           width: '100%',
@@ -65,24 +65,24 @@ export default function Home({ navigation }) {
 
     <View style={{height:100, width:'100%', borderRadius:10, backgroundColor: '#FF8C8C', marginTop:20, flexDirection: 'row', justifyContent: 'space-between'}}>
       <View>
-        <Text style={{color:'white', padding:15, paddingBottom:10, fontSize:20, fontFamily: 'chulaBold'}}>รอรับเรื่อง</Text>
-        <Text style={{color:'white', paddingLeft:15, fontSize:20, fontFamily: 'chulaBold'}}>123(100%)</Text>
+        <Text style={{color:'white', padding:15, paddingBottom:10, fontSize:20, fontFamily: ''}}>รอรับเรื่อง</Text>
+        <Text style={{color:'white', paddingLeft:15, fontSize:20, fontFamily: ''}}>123(100%)</Text>
       </View>
       <View style={{backgroundColor:'black', height:80 , width:80, marginHorizontal:15, marginVertical:10 , borderRadius:9}}></View>
     </View>
 
     <View style={{height:100, width:'100%', borderRadius:10, backgroundColor: '#FFDAAF',marginTop:20 , paddingBottom:10, flexDirection: 'row', justifyContent: 'space-between'}}>
       <View>
-        <Text style={{color:'white', padding:15, fontSize:20, fontFamily: 'chulaBold'}}>กำลังดำเนินการ</Text>
-        <Text style={{color:'white', paddingLeft:15, fontSize:20, fontFamily: 'chulaBold'}}>123(100%)</Text>
+        <Text style={{color:'white', padding:15, fontSize:20, fontFamily: ''}}>กำลังดำเนินการ</Text>
+        <Text style={{color:'white', paddingLeft:15, fontSize:20, fontFamily: ''}}>123(100%)</Text>
       </View>
       <View style={{backgroundColor:'black', height:80 , width:80, marginHorizontal:15, marginVertical:10, borderRadius:9}}></View>
     </View>
 
     <View style={{height:100, width:'100%', borderRadius:10, backgroundColor: '#A1E0A7', marginTop:20, paddingBottom:10, flexDirection: 'row', justifyContent: 'space-between'}}>
       <View>
-        <Text style={{color:'white', padding:15, fontSize:20, fontFamily: 'chulaBold'}}>เสร็จสิ้น</Text>
-        <Text style={{color:'white', paddingLeft:15, fontSize:20, fontFamily: 'chulaBold'}}>123(100%)</Text>
+        <Text style={{color:'white', padding:15, fontSize:20, fontFamily: ''}}>เสร็จสิ้น</Text>
+        <Text style={{color:'white', paddingLeft:15, fontSize:20, fontFamily: ''}}>123(100%)</Text>
       </View>
       <View style={{backgroundColor:'black', height:80 , width:80, marginHorizontal:15, marginVertical:10, borderRadius:9}}></View>
     </View>
@@ -90,27 +90,27 @@ export default function Home({ navigation }) {
 </View>
 
     <View style={{paddingTop:40}}>
-      <Text style={{fontFamily: 'chulaBold', fontSize:25, color:'#E26199'}}>ปัญหาที่รับแจ้ง: </Text>
+      <Text style={{fontFamily: '', fontSize:25, color:'#E26199'}}>ปัญหาที่รับแจ้ง: </Text>
       <View style={{paddingTop:8, flexDirection:'row'}}>
         <View style={{width: '50%'}}>
-      <Text style={{fontFamily: 'chulaBold', fontSize:20, color:'#E26199',paddingTop:10}}>ถนน</Text>
-      <Text style={{fontFamily: 'chulaBold', fontSize:20, color:'#E26199',paddingTop:10}}>ความสะอาด</Text>
-      <Text style={{fontFamily: 'chulaBold', fontSize:20, color:'#E26199',paddingTop:10}}>การจราจร</Text>
-      <Text style={{fontFamily: 'chulaBold', fontSize:20, color:'#E26199',paddingTop:10}}>ไฟฟ้า</Text>
-      <Text style={{fontFamily: 'chulaBold', fontSize:20, color:'#E26199',paddingTop:10}}>น้ำท่วม</Text>
-      <Text style={{fontFamily: 'chulaBold', fontSize:20, color:'#E26199',paddingTop:10}}>ต้นไม้</Text>
-      <Text style={{fontFamily: 'chulaBold', fontSize:20, color:'#E26199',paddingTop:10}}>ทางเท้า</Text>
-      <Text style={{fontFamily: 'chulaBold', fontSize:20, color:'#E26199',paddingTop:10}}>เสียงรบกวน</Text>
+      <Text style={{fontFamily: '', fontSize:20, color:'#E26199',paddingTop:10}}>ถนน</Text>
+      <Text style={{fontFamily: '', fontSize:20, color:'#E26199',paddingTop:10}}>ความสะอาด</Text>
+      <Text style={{fontFamily: '', fontSize:20, color:'#E26199',paddingTop:10}}>การจราจร</Text>
+      <Text style={{fontFamily: '', fontSize:20, color:'#E26199',paddingTop:10}}>ไฟฟ้า</Text>
+      <Text style={{fontFamily: '', fontSize:20, color:'#E26199',paddingTop:10}}>น้ำท่วม</Text>
+      <Text style={{fontFamily: '', fontSize:20, color:'#E26199',paddingTop:10}}>ต้นไม้</Text>
+      <Text style={{fontFamily: '', fontSize:20, color:'#E26199',paddingTop:10}}>ทางเท้า</Text>
+      <Text style={{fontFamily: '', fontSize:20, color:'#E26199',paddingTop:10}}>เสียงรบกวน</Text>
       </View>
       <View style={{width: '50%'}}>
-      <Text style={{fontFamily: 'chulaBold', fontSize:20, color:'#E26199',paddingTop:10}}>อุปกรณ์เสียหาย</Text>
-      <Text style={{fontFamily: 'chulaBold', fontSize:20, color:'#E26199',paddingTop:10}}>อาคารชำรุด</Text>
-      <Text style={{fontFamily: 'chulaBold', fontSize:20, color:'#E26199',paddingTop:10}}>สายสื่อสาร</Text>
-      <Text style={{fontFamily: 'chulaBold', fontSize:20, color:'#E26199',paddingTop:10}}>ฝาท่อระบายน้ำ</Text>
-      <Text style={{fontFamily: 'chulaBold', fontSize:20, color:'#E26199',paddingTop:10}}>กลิ่นควัน</Text>
-      <Text style={{fontFamily: 'chulaBold', fontSize:20, color:'#E26199',paddingTop:10}}>สัตว์</Text>
-      <Text style={{fontFamily: 'chulaBold', fontSize:20, color:'#E26199',paddingTop:10}}>น้ำประปา</Text>
-      <Text style={{fontFamily: 'chulaBold', fontSize:20, color:'#E26199',paddingTop:10}}>อื่นๆ</Text>
+      <Text style={{fontFamily: '', fontSize:20, color:'#E26199',paddingTop:10}}>อุปกรณ์เสียหาย</Text>
+      <Text style={{fontFamily: '', fontSize:20, color:'#E26199',paddingTop:10}}>อาคารชำรุด</Text>
+      <Text style={{fontFamily: '', fontSize:20, color:'#E26199',paddingTop:10}}>สายสื่อสาร</Text>
+      <Text style={{fontFamily: '', fontSize:20, color:'#E26199',paddingTop:10}}>ฝาท่อระบายน้ำ</Text>
+      <Text style={{fontFamily: '', fontSize:20, color:'#E26199',paddingTop:10}}>กลิ่นควัน</Text>
+      <Text style={{fontFamily: '', fontSize:20, color:'#E26199',paddingTop:10}}>สัตว์</Text>
+      <Text style={{fontFamily: '', fontSize:20, color:'#E26199',paddingTop:10}}>น้ำประปา</Text>
+      <Text style={{fontFamily: '', fontSize:20, color:'#E26199',paddingTop:10}}>อื่นๆ</Text>
       </View>
       
       </View>
