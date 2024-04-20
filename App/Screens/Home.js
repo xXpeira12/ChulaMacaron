16e0c6e2b1838ea870530a4118ad353178773663
Amphi <6633287021@student.chula.ac.th>
2024-04-20 17:15:17 +0700

    change font in Home

--- App/Screens/Home.js
+++ App/Screens/Home.js
@@ -18,16 +18,18 @@ export default function Home({ navigation }) {
     <ScrollView style={{backgroundColor:'white'}}>
     <View style={{backgroundColor:'white', padding:50}}>
      
-    <Text style={{fontSize:30, textAlign:'center' , fontWeight:'bold' , paddingTop:15, color:'#E26199'}}>CHULA MACARON</Text>
-    <Text style={{fontSize:16, textAlign:'center', fontWeight:'bold', color:'#E26199'}}>make Nisit's life better</Text>
+
+    <Image source={Macaron} style={{width:'100%', resizeMode:'center'}}></Image>
+    <Text style={{fontSize:30, textAlign:'center' ,fontFamily: 'chulaBold' , paddingTop:0, color:'#E26199'}}>CHULA MACARON</Text>
+    <Text style={{fontSize:16, textAlign:'center', fontFamily: 'chulaReg', color:'#E26199'}}>make Nisits' lives better</Text>
 
     <View style={{flexDirection: screenWidth > 769 ? 'row' : 'column'}}>
     <View style={{width: screenWidth > 769 ? '50%' : '100%',}}>
-    <Text style= {{paddingTop:20, color:'#E26199'}}>You can report any problems around Chulalongkorn University.</Text>
+    {/* <Text style= {{paddingTop:20, color:'#E26199', fontFamily: 'chulaBold'}}>You can report any problems around Chulalongkorn University.</Text> */}
 
     <View style= {{paddingTop:20}}>
-      <Text style= {{fontWeight:'bold' , color:'#E26199' ,fontSize:25}}>แพลตฟอร์ม</Text>
-      <Text style= {{fontWeight:'bold' , color:'#E26199' ,fontSize:25, paddingBottom:10}}>แจ้งและจัดการปัญหาในจุฬา</Text>
+      <Text style= {{fontFamily: 'chulaBold' , color:'#E26199' ,fontSize:25}}>แพลตฟอร์ม</Text>
+      <Text style= {{fontFamily: 'chulaBold' , color:'#E26199' ,fontSize:25, paddingBottom:10}}>แจ้งและจัดการปัญหาในจุฬา</Text>
     </View>
 
     <TouchableOpacity onPress={() => {}} style={{ backgroundColor: '#E26199', padding: 20, alignItems: 'center',  borderRadius: 10 ,
@@ -41,12 +43,12 @@ export default function Home({ navigation }) {
     elevation: 5,
     width: screenWidth > 769 ? '40%' : '100%',
   }}>
-    <Text style={{ color: 'white' }}>แจ้งปัญหาได้ที่นี่ (Click Here)</Text>
+    <Text style={{ color: 'white', fontFamily: 'chulaBold', fontSize: 18 }}>แจ้งปัญหาได้ที่นี่ (Click Here)</Text>
     </TouchableOpacity>
     </View>
 
     <View style={{flexDirection: screenWidth > 769 ? 'column':'column' , paddingTop:20 , width: screenWidth > 769 ? '50%' : '100%'}}>
-      <Text style={{fontWeight:'bold',fontSize:25 , color:'#E26199', paddingBottom:20}}>OverView</Text>
+      <Text style={{fontFamily: 'chulaBold' ,fontSize:25 , color:'#E26199', paddingBottom:10}}>Overview</Text>
   
       <View style={{
           width: '100%',
@@ -63,56 +65,56 @@ export default function Home({ navigation }) {
 
     <View style={{height:100, width:'100%', borderRadius:10, backgroundColor: '#FF8C8C', marginTop:20, flexDirection: 'row', justifyContent: 'space-between'}}>
       <View>
-        <Text style={{color:'white', padding:15, paddingBottom:10, fontSize:20, fontWeight:'bold'}}>รอรับเรื่อง</Text>
-        <Text style={{color:'white', paddingLeft:15, fontSize:20, fontWeight:'bold'}}>123(100%)</Text>
+        <Text style={{color:'white', padding:15, paddingBottom:10, fontSize:20, fontFamily: 'chulaBold'}}>รอรับเรื่อง</Text>
+        <Text style={{color:'white', paddingLeft:15, fontSize:20, fontFamily: 'chulaBold'}}>123(100%)</Text>
       </View>
       <View style={{backgroundColor:'black', height:80 , width:80, marginHorizontal:15, marginVertical:10 , borderRadius:9}}></View>
     </View>
 
     <View style={{height:100, width:'100%', borderRadius:10, backgroundColor: '#FFDAAF',marginTop:20 , paddingBottom:10, flexDirection: 'row', justifyContent: 'space-between'}}>
       <View>
-        <Text style={{color:'white', padding:15, fontSize:20, fontWeight:'bold'}}>ดำเนินการ</Text>
-        <Text style={{color:'white', paddingLeft:15, fontSize:20, fontWeight:'bold'}}>123(100%)</Text>
+        <Text style={{color:'white', padding:15, fontSize:20, fontFamily: 'chulaBold'}}>ดำเนินการ</Text>
+        <Text style={{color:'white', paddingLeft:15, fontSize:20, fontFamily: 'chulaBold'}}>123(100%)</Text>
       </View>
       <View style={{backgroundColor:'black', height:80 , width:80, marginHorizontal:15, marginVertical:10, borderRadius:9}}></View>
     </View>
 
     <View style={{height:100, width:'100%', borderRadius:10, backgroundColor: '#A1E0A7', marginTop:20, paddingBottom:10, flexDirection: 'row', justifyContent: 'space-between'}}>
       <View>
-        <Text style={{color:'white', padding:15, fontSize:20, fontWeight:'bold'}}>เสร็จสิ้น</Text>
-        <Text style={{color:'white', paddingLeft:15, fontSize:20, fontWeight:'bold'}}>123(100%)</Text>
+        <Text style={{color:'white', padding:15, fontSize:20, fontFamily: 'chulaBold'}}>เสร็จสิ้น</Text>
+        <Text style={{color:'white', paddingLeft:15, fontSize:20, fontFamily: 'chulaBold'}}>123(100%)</Text>
       </View>
       <View style={{backgroundColor:'black', height:80 , width:80, marginHorizontal:15, marginVertical:10, borderRadius:9}}></View>
     </View>
 
 </View>
 
-    <View style={{paddingTop:30}}>
-      <Text style={{fontWeight:'bold',fontSize:25, color:'#E26199'}}>KIND OF REPORT ISSUES WE ACCEPT</Text>
-      <View style={{paddingTop:15, flexDirection:'row'}}>
+    <View style={{paddingTop:40}}>
+      <Text style={{fontFamily: 'chulaBold', fontSize:25, color:'#E26199'}}>ปัญหาที่รับแจ้ง: </Text>
+      <View style={{paddingTop:8, flexDirection:'row'}}>
         <View style={{width: '50%'}}>
-      <Text style={{fontWeight:'bold',fontSize:20, color:'#E26199',paddingTop:10}}>ถนน</Text>
-      <Text style={{fontWeight:'bold',fontSize:20, color:'#E26199',paddingTop:10}}>ความสะอาด</Text>
-      <Text style={{fontWeight:'bold',fontSize:20, color:'#E26199',paddingTop:10}}>การจราจร</Text>
-      <Text style={{fontWeight:'bold',fontSize:20, color:'#E26199',paddingTop:10}}>ไฟฟ้า</Text>
-      <Text style={{fontWeight:'bold',fontSize:20, color:'#E26199',paddingTop:10}}>น้ำท่วม</Text>
-      <Text style={{fontWeight:'bold',fontSize:20, color:'#E26199',paddingTop:10}}>ต้นไม้</Text>
-      <Text style={{fontWeight:'bold',fontSize:20, color:'#E26199',paddingTop:10}}>ทางเท้า</Text>
-      <Text style={{fontWeight:'bold',fontSize:20, color:'#E26199',paddingTop:10}}>เสียงรบกวน</Text>
+      <Text style={{fontFamily: 'chulaBold', fontSize:20, color:'#E26199',paddingTop:10}}>ถนน</Text>
+      <Text style={{fontFamily: 'chulaBold', fontSize:20, color:'#E26199',paddingTop:10}}>ความสะอาด</Text>
+      <Text style={{fontFamily: 'chulaBold', fontSize:20, color:'#E26199',paddingTop:10}}>การจราจร</Text>
+      <Text style={{fontFamily: 'chulaBold', fontSize:20, color:'#E26199',paddingTop:10}}>ไฟฟ้า</Text>
+      <Text style={{fontFamily: 'chulaBold', fontSize:20, color:'#E26199',paddingTop:10}}>น้ำท่วม</Text>
+      <Text style={{fontFamily: 'chulaBold', fontSize:20, color:'#E26199',paddingTop:10}}>ต้นไม้</Text>
+      <Text style={{fontFamily: 'chulaBold', fontSize:20, color:'#E26199',paddingTop:10}}>ทางเท้า</Text>
+      <Text style={{fontFamily: 'chulaBold', fontSize:20, color:'#E26199',paddingTop:10}}>เสียงรบกวน</Text>
       </View>
       <View style={{width: '50%'}}>
-      <Text style={{fontWeight:'bold',fontSize:20, color:'#E26199',paddingTop:10}}>อุปกรณ์เสียหาย</Text>
-      <Text style={{fontWeight:'bold',fontSize:20, color:'#E26199',paddingTop:10}}>อาคารชำรุด</Text>
-      <Text style={{fontWeight:'bold',fontSize:20, color:'#E26199',paddingTop:10}}>สายสื่อสาร</Text>
-      <Text style={{fontWeight:'bold',fontSize:20, color:'#E26199',paddingTop:10}}>ฝาท่อระบายน้ำ</Text>
-      <Text style={{fontWeight:'bold',fontSize:20, color:'#E26199',paddingTop:10}}>กลิ่นควัน</Text>
-      <Text style={{fontWeight:'bold',fontSize:20, color:'#E26199',paddingTop:10}}>สัตว์</Text>
-      <Text style={{fontWeight:'bold',fontSize:20, color:'#E26199',paddingTop:10}}>น้ำประปา</Text>
-      <Text style={{fontWeight:'bold',fontSize:20, color:'#E26199',paddingTop:10}}>อื่นๆ</Text>
+      <Text style={{fontFamily: 'chulaBold', fontSize:20, color:'#E26199',paddingTop:10}}>อุปกรณ์เสียหาย</Text>
+      <Text style={{fontFamily: 'chulaBold', fontSize:20, color:'#E26199',paddingTop:10}}>อาคารชำรุด</Text>
+      <Text style={{fontFamily: 'chulaBold', fontSize:20, color:'#E26199',paddingTop:10}}>สายสื่อสาร</Text>
+      <Text style={{fontFamily: 'chulaBold', fontSize:20, color:'#E26199',paddingTop:10}}>ฝาท่อระบายน้ำ</Text>
+      <Text style={{fontFamily: 'chulaBold', fontSize:20, color:'#E26199',paddingTop:10}}>กลิ่นควัน</Text>
+      <Text style={{fontFamily: 'chulaBold', fontSize:20, color:'#E26199',paddingTop:10}}>สัตว์</Text>
+      <Text style={{fontFamily: 'chulaBold', fontSize:20, color:'#E26199',paddingTop:10}}>น้ำประปา</Text>
+      <Text style={{fontFamily: 'chulaBold', fontSize:20, color:'#E26199',paddingTop:10}}>อื่นๆ</Text>
       </View>
       
       </View>
-      <Image source={Macaron} style={{width:'100%', resizeMode:'center'}}></Image>
+  
 
     </View>
       
